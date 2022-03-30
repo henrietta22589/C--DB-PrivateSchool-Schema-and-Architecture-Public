777ee97b0b1dedc7d5eb191129ad8ff3766d9231
henrietta22589 <93531231+henrietta22589@users.noreply.github.com>
2022-03-30 21:12:55 +0300

    Add files via upload

---
+++ henriettasschool1/AddStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace henriettasschool1
+{
+     public class AddStudent
+    {
+        eriettaDB2Entities db = new eriettaDB2Entities();
+        public void Add()
+        {
+            Console.WriteLine("Give me the ID");
+            string id_OfStudent = Console.ReadLine();
+            int id1=Convert.ToInt32(id_OfStudent);
+            var Student = db.Students.Find(id1);
+            db.Entry(Student).State = EntityState.Added;
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ henriettasschool1/AddTrainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace henriettasschool1
+{
+    public class AddTrainer
+    {
+        eriettaDB2Entities db = new eriettaDB2Entities();
+
+        public void TrainersAdded()
+        {
+            Console.WriteLine("Give me the name of the trainer, you want to Add in the List");
+            string Name_of_Trainer = Console.ReadLine();
+            Console.WriteLine("The First Name of the trainer is"+ Name_of_Trainer);
+            Console.WriteLine("Give me the Last_name of the trainer, you want to Add in the List");
+            string Last_of_Trainer = Console.ReadLine();
+            Console.WriteLine("The First Name of the trainer is" + Last_of_Trainer);
+            Trainer t_1 = new Trainer()
+            {
+                FirstName = Name_of_Trainer,
+                LastName = Last_of_Trainer
+            };
+            db.Trainers.Add(t_1);
+            db.SaveChanges();
+        }
+
+
+    }
+}
